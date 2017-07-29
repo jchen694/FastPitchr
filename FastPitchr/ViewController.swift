295ac2dcd94d7b615f9ad3492885225de93b1a9a
juliachen <jchen694@gatech.edu>
2017-07-28 21:57:23 -0700

    first commit

--- FastPitchr/ViewController.swift
+++ FastPitchr/ViewController.swift
@@ -1,25 +1,65 @@
 //
 //  ViewController.swift
-//  FastPitchr
+//  FastPitch
 //
 //  Created by Julia Chen on 7/28/17.
 //  Copyright © 2017 Julia Chen. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: CALayer!
+    
+    var captureDevice: AVCaptureDevice!
+    
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        if let availableDevices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .back).devices {
+            captureDevice = availableDevices.first
+            beginSession()
+        }
+    }
+    
+    func beginSession() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            self.previewLayer = previewLayer
+            self.view.layer.addSublayer(self.previewLayer)
+            self.previewLayer.frame = self.view.layer.frame
+            captureSession.startRunning()
+            
+            let dataOutput = AVCaptureVideoDataOutput()
+            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)]
+            
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            
+            if captureSession.canAddOutput(dataOutput) {
+                captureSession.addOutput(dataOutput)
+            }
+            
+            captureSession.commitConfiguration()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
