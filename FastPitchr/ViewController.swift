aad50f4e223f472ba439536af2be541f35a9f817
juliachen <jchen694@gatech.edu>
2017-07-28 21:51:20 -0700

    Initial Commit

---
+++ FastPitchr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastPitchr
+//
+//  Created by Julia Chen on 7/28/17.
+//  Copyright © 2017 Julia Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
